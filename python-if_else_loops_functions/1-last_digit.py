6978ce63e82d2d8ac5ae718354665c75e1785a3f
William <wilguidro@gmail.com>
2024-07-22 10:13:50 +0000

    Last digits updated

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 
 # Extract the last digit
-last_digit =  number % 10
+last_digit = number % 10
 if number < 0:
-    last_digit = -(- number % 10)
+    last_digit = -(-number % 10)
 
 # Determine the appropriate message
 if last_digit > 5:
