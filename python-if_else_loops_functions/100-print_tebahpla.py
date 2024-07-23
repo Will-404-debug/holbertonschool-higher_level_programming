086c8a65b793ac61add0f1d4dcfe6e2180991c7f
William <wilguidro@gmail.com>
2024-07-23 11:43:51 +0000

    Smile in the mirror updated

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
 def print_tebahpla():
-    # Loop to print the alphabet in reverse order
+    # Initialize an empty string to hold the result
+    result = ''
+    # Loop to build the string of the alphabet in reverse order
     for i in range(26):
         # Determine if the position is even or odd
         if i % 2 == 0:
-            # For even indices, print lowercase letters starting from 'z'
-            print(chr(122 - (i // 1)), end='')
+            # For even indices, add lowercase letters starting from 'z'
+            result += chr(122 - (i // 1))
         else:
-            # For odd indices, print uppercase letters starting from 'Y'
-            print(chr(90 - (i // 1)), end='')
+            # For odd indices, add uppercase letters starting from 'Y'
+            result += chr(90 - (i // 1))
+    # Print the final result string
+    print(result)
 
 print_tebahpla()
