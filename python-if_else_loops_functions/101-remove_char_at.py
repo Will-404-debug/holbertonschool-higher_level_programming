fef73429eff919a857f7ac14fd11c3ac4234e7bb
William <wilguidro@gmail.com>
2024-07-23 12:07:41 +0000

    Remove at position

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def remove_char_at(s, n):
+    if n < 0 or n >= len(s):
+        return s
+    return s[:n] + s[n+1:]
+
+# You can test the function with the provided examples
+if __name__ == "__main__":
+    print(remove_char_at("Best School", 3))  # "Bes School"
+    print(remove_char_at("Chicago", 2))      # "Chcago"
+    print(remove_char_at("C is fun!", 0))    # " is fun!"
+    print(remove_char_at("School", 10))      # "School"
+    print(remove_char_at("Python", -2))      # "Python"
