a107d0a93208fa07ac05eb46a62d77ba50829873
William <wilguidro@gmail.com>
2024-07-24 10:11:37 +0000

    Coordinates of a square updated

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -100,4 +100,4 @@ class Square:
             return
         print("\n" * self.__position[1], end="")
         for _ in range(self.__size):
-            print("_" * self.__position[0] + "#" * self.__size)
+            print(" " * self.__position[0] + "#" * self.__size)
