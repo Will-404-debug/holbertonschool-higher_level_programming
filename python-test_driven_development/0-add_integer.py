5897b08a420ab889d1f0721ef1a4dca21eb0ce5a
Will-404-debug <wilguidro@gmail.com>
2024-07-25 12:10:45 +0000

    Integers addition up updated

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,17 +3,34 @@
 """
 Module for adding two integers.
 
-This module provides a function `add_integer` that adds two numbers
+This module provides a function add_integer() that adds two numbers
 and returns the result as an integer. It performs type checking and
 casts float inputs to integers.
+
+>>> add_integer(1, 2)
+3
+>>> add_integer(100, -2)
+98
+>>> add_integer(2)
+100
+>>> add_integer(100.3, -2)
+98
+>>> add_integer(4, "School")
+Traceback (most recent call last):
+    ...
+TypeError: b must be an integer
+>>> add_integer(None)
+Traceback (most recent call last):
+    ...
+TypeError: a must be an integer
 """
 
 def add_integer(a, b=98):
     """Add two integers.
 
     Parameters:
-    a (int, float): The first number to be added.
-    b (int, float): The second number to be added, default is 98.
+    a -- The first number to be added.
+    b --  The second number to be added, default is 98.
 
     Returns:
     int: The sum of a and b after casting them to integers.
@@ -27,3 +44,7 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     
     return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
